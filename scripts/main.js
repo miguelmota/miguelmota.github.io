1f80b1bcc9efd1ae63e5fd931bfebf6716ac595f
Miguel Mota <miguelmota2@gmail.com>
2011-03-31 12:55:15 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -66,7 +66,7 @@ $(document).ajaxComplete(function(){
 $("nav.main a:not('a#blog')").live('click', function(){
 	c = 0;
 	var toLoad = $(this).attr('href')+' section.content';
-	window.location.hash = '!'+$(this).attr('href').substr(10,$(this).attr('href').length);
+	window.location.hash = '!'+$(this).attr('href').substr(23,$(this).attr('href').length);
 	$('section.content').fadeOut('fast',loadContent);
 	$('div.loader').fadeIn('normal');
 	function loadContent(){
