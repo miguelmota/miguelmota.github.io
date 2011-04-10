771e4b9d0de2a6c781183160f6328f4e17c5ccd6
Miguel Mota <miguelmota2@gmail.com>
2011-04-10 01:50:14 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -143,7 +143,7 @@ $('div.posti h3 a').live('click', function(){
 	$('div.loader').fadeIn('normal');
 	var stateObj2 = { foo2:  $(this).attr('href') };
 	history.pushState(stateObj2, "Title2", $(this).attr('href'));
-	var title2 = $('div.posti h3 a').text()+$('div.theTitle').text();
+	var title2 = $(this).text()+$('div.theTitle').text();
 	document.title = title2;
 	function loadContent2(){
 		$('div.posti').load(toLoad2,showNewContent2);
