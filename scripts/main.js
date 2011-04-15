ac65edbd143aa9e5555730fbbe74642dc4cbf925
Miguel Mota <miguelmota2@gmail.com>
2011-04-15 14:04:24 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -171,11 +171,19 @@ $('div.posti h3 a').live('click', function(){
 function loadDisqus(){
     var disqus_shortname = 'miguelmota';
     var disqus_url = 'http://wwww.miguelmota.com/{{ page.url }}';
+    //comment box
     (function() {
         var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
         dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
     })();
+    //comment count
+    (function () {
+        var s = document.createElement('script'); s.async = true;
+        s.type = 'text/javascript';
+        s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('body')[0]).appendChild(s);
+    }());
 }
 var position = 0;
 var length = 'portfolio'.length;
