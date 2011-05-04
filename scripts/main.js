01b520e165a5aa0293ec5b4cb8cc8b6c01d2bdc1
Miguel Mota <miguelmota2@gmail.com>
2011-05-04 13:55:42 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -182,7 +182,7 @@ var bc = 0;
 
 function loadPage(){
 	
-	var toLoad = '/'+window.location.hash.substr(2)+' section.content';
+	var toLoad = '/'+window.location.hash.substr(1)+' section.content';
 	//window.location.hash = window.location.hash.substr(1);
 	$('section.content').fadeOut('fast',loadContent);
 	$('div.loader').fadeIn('normal');
