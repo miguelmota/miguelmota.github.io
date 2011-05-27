b704d1e4c895607342eef06123e592955b014741
Miguel Mota <miguelmota2@gmail.com>
2011-05-26 17:46:12 -0700

    ...

--- mobile/scripts/main.js
+++ mobile/scripts/main.js
@@ -50,7 +50,7 @@ $(document).ready(function(){
 		$('#article div.content').load($(this).attr('href')+' section.content');
 	
 		
-	}).removeAttr('rel');
+	})..attr('href', '#article').removeAttr('rel');
 
 
 });
