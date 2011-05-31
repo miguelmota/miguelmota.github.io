abdf563cae94f9e5df36dff1072b04ca5ed27c8b
Miguel Mota <miguelmota2@gmail.com>
2011-05-31 15:01:17 -0700

    ...

--- plugins/mtip/mtip.js
+++ plugins/mtip/mtip.js
@@ -8,8 +8,8 @@
 	$.fn.mtip = function() {
 		$(this).wrap('<div class="tooltip_container" />');
 		$(this).hover(function() {
-					$(this).attr('mtip', $(this).attr('title')).removeAttr('title');
-					var title = $(this).attr('mtip');
+					//$(this).attr('mtip', $(this).attr('title')).removeAttr('title');
+					var title = $(this).attr('title');
 					$(this).after("<div class='tooltip'></div>");
 					$('div.tooltip').text(title).hide();
 					$('div.tooltip').css({
