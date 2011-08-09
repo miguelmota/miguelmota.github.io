5a9d244bbfa7df50d01f258702eaf13597b30f99
Miguel Mota <miguelmota2@gmail.com>
2011-08-09 14:17:54 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -540,7 +540,7 @@ function portfolioPage(){
 	
 	// Show zoom icon on hover
 	$('a.fancybox').hover(function(){
-			jQuery('span.zoom-icon, this').css('display', 'block');
+			jQuery('span.zoom-icon', this).css('display', 'block');
 	},function(){
 			jQuery('span.zoom-icon', this).hide();
 		  }
