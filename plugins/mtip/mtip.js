a3b46853c89941b8a359cb4bdd766265ec584546
Miguel Mota <miguelmota2@gmail.com>
2011-08-14 00:40:21 -0700

    ...

--- plugins/mtip/mtip.js
+++ plugins/mtip/mtip.js
@@ -12,10 +12,10 @@
 			var title = $(this).attr('mtip');
 			$(this).wrap("<div class='tooltip-container' />");
 			$('div.tooltip-container').prepend("<div class='tooltip' />");
-			$(this).hover(function() {
+			$('div.tooltip-container').hover(function() {
 						$('div.tooltip').text(title).stop(true, true).animate({opacity: 'show', top: '-30'}, 'slow');
 				}, function() {
-						jQuery('div.tooltip', this).animate({opacity: 'hide', top: '-20'}, 'fast');
+						$('div.tooltip').animate({opacity: 'hide', top: '-20'}, 'fast');
 			});	
 		});
 
