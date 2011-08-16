054d6546cf9e77d8006871b5257883f58d35ee0b
Miguel Mota <miguelmota2@gmail.com>
2011-08-15 17:24:10 -0700

    ...

--- mobile/scripts/main.js
+++ mobile/scripts/main.js
@@ -127,10 +127,10 @@ var jQt = $.jQTouch({
 
 function loadDisqus(){
 	
+	//comment form
     var disqus_shortname = 'miguelmota';
-    var disqus_url = 'http://wwww.miguelmota.com/{{ page.url }}';
-    
-    //comment box
+    var disqus_identifier = '{{ page.url }}';
+    var disqus_url = 'http://www.miguelmota.com{{ page.url }}';
     (function() {
         var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
         dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
@@ -138,11 +138,12 @@ function loadDisqus(){
     })();
     
     //comment count
+    var disqus_shortname = 'miguelmota';
     (function () {
         var s = document.createElement('script'); s.async = true;
         s.type = 'text/javascript';
         s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(s);
+        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
     }());
 }
 
