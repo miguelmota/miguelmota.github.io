8447ea0cafc83b99d2e7c68ec8cf97e864afa564
Miguel Mota <miguelmota2@gmail.com>
2011-08-17 22:40:17 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -658,11 +658,12 @@ function blogPage(){
 	$.getScript('http://platform.twitter.com/widgets.js');
 	
 	// Initiatlize Google Plus One
-	(function() {
+	/*(function() {
 		  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
 		  po.src = 'https://apis.google.com/js/plusone.js';
 		  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
 		})();
+	*/
 	
 	// Initialize Tumblr share button
 	$.getScript('http://platform.tumblr.com/v1/share.js');
