7ca135f8979960e99b085a75800d86cba3d36de3
Miguel Mota <miguelmota2@gmail.com>
2011-08-17 22:47:24 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -683,24 +683,6 @@ function blogPage(){
 }
 
 function loadDisqus(){
-	
-    var disqus_shortname = 'miguelmota';
-    var disqus_url = 'http://wwww.miguelmota.com/{{ page.url }}';
-    
-    //comment count
-    (function () {
-        var s = document.createElement('script'); s.async = true;
-        s.type = 'text/javascript';
-        s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(s);
-    }());
-    
-    //comment box
-    (function() {
-        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-    })();
 }
 
 //load Google Search function
