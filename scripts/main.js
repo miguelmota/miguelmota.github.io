3455ce927de1a8ef3a2060afe621caea624c3c2f
Miguel Mota <miguelmota2@gmail.com>
2011-08-23 00:27:19 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -217,7 +217,30 @@ $(document).ajaxComplete(function(){
 		default:
 			break;
 	}*/
-
+	
+	// add selected class based on the section block class
+	$('ul.main-nav li a').each(function(){ 
+		if($(this).attr('id') == $('section.content').attr('class').split(' ')[1]){
+			$(this).addClass('selected');
+			switch($('section.content').attr('class').split(' ')[1]){
+				case 'stream':
+					streamPage();
+					break;
+				case 'about':
+					break;
+				case 'portfolio':
+					portfolioPage();
+					break;
+				case 'contact':
+					contactPage();
+					break;
+				case 'blog':
+					blogPage();
+				default:
+					break;
+			}
+		}
+	});
 });
 
 //global variables
