89cd7f93c8ca4517c87ddcddc910d14d19688d34
Miguel Mota <miguelmota2@gmail.com>
2011-08-25 19:43:08 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -673,8 +673,8 @@ function contactPage(){
 		var str = $('form.contact-form').serialize();
 		$.ajax({
 			type: 'get',
-			//url: 'http://www.foodfail.org/miguelmota/contact.php',
-			url: 'http://miguelmota.webuda.com/contact/contact.php',
+			url: 'http://www.foodfail.org/miguelmota/contact.php',
+			//rl: 'http://miguelmota.webuda.com/contact/contact.php',
 			data: str,
 			success: function(){
 				success();
