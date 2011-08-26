e1cd110eb14aea9051e146e77d2aec6ee163f331
Miguel Mota <miguelmota2@gmail.com>
2011-08-26 00:57:39 -0700

    ...

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@ define("WEBMASTER_EMAIL", 'miguelmota2@gmail.com');
 $name = stripslashes($_POST['name']);
 $email = stripslashes($_POST['email']);
 $subject = 'Contact Form | Miguel Mota';
-$message = 'From: '.$name.' <'.$email."> \n Message9: \n".stripslashes($_POST['message']);
+$message = 'From: '.$name.' <'.$email.">\nMessage: \n".stripslashes($_POST['message']);
 
 mail(WEBMASTER_EMAIL, $subject, $message,
      "From: ".$name." <".$email.">rn"
