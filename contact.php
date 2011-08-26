b3ef0a2f1784e74d2771bd9ca4f964fac4b3b25b
Miguel Mota <miguelmota2@gmail.com>
2011-08-26 00:55:34 -0700

    ...

--- contact.php
+++ contact.php
@@ -3,9 +3,9 @@ header('Access-Control-Allow-Origin: *');
 define("WEBMASTER_EMAIL", 'miguelmota2@gmail.com');
 
 $name = stripslashes($_POST['name']);
-$email = $_POST['email'];
-$subject = 'contact form';
-$message = 'From: '.$name.' <'.$email.'>\r\n Message:\r\n'.stripslashes($_POST['message']);
+$email = stripslashes($_POST['email']);
+$subject = 'Contact Form | Miguel Mota';
+$message = 'From: '.$name.' <'.$email."> \n Message9: \n".stripslashes($_POST['message']);
 
 mail(WEBMASTER_EMAIL, $subject, $message,
      "From: ".$name." <".$email.">rn"
