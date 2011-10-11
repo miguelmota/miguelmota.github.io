dfeec8bd4c89e89d870d992968404761553dcc17
Miguel Mota <miguelmota2@gmail.com>
2011-10-11 16:23:35 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -420,6 +420,7 @@ function streamPage(){
 			    	    $("li.status[id='"+id+"'] span").find(":contains('http')").wrapInner("<a href='"+$("li.status[id='"+id+"'] span").find(":contains('http')").text()+"'> </a>");
 			    	    $("li.status[id='"+id+"'] span").find(":contains('@')").wrapInner("<a href='http://twitter.com/"+$("li.status[id='"+id+"'] span").find(":contains('@')").text().substr(1)+"'> </a>");
 			    	    $("li.status[id='"+id+"'] span").find(":contains('#')").wrapInner("<a href='http://twitter.com/#!/search/"+$("li.status[id='"+id+"'] span").find(":contains('#')").text().substr(1)+"'> </a>");
+			    	    $("div.twitter").html(data[0].text);
 					});
 					$('div.twitter div.loader').css('display','none');
 					showMtip('a.social.twitter');
