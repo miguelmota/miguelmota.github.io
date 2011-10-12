8c6471625cf4073829369bf76c7f14868f6f819f
Miguel Mota <miguelmota2@gmail.com>
2011-10-11 17:42:52 -0700

    Edited scripts/main.js via GitHub

--- scripts/main.js
+++ scripts/main.js
@@ -415,27 +415,11 @@ function streamPage(){
 						var post = status.text;
 						var id = status.id_str;
 			    	  	var date = new Date(status.created_at).toUTCString();
-				        var newText = '<span>'+post.split(' ').join('</span> <span>')+'</span>';
-				     /*  String.prototype.parseURL = function() {
-						return this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&~\?\/.=]+/g, function(url) {
-						return url.link(url);
-					});
-					String.prototype.parseUsername = function() {
-						return this.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
-							var username = u.replace("@","")
-							return u.link("http://twitter.com/"+username);
-						});
-					String.prototype.parseHashtag = function() {
-					return this.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
-						var tag = t.replace("#","%23")
-						return t.link("http://search.twitter.com/search?q="+tag);
-					}); */
-
-			    	  $('ul.twitter-status').append("<li id='"+id+"' class='status'>&#187; <span class='post'>"+newText+"</span> <span class='stream-date'><a href='http://twitter.com/miguel_mota/status/"+id+"'>"+niceTime(date)+"</a></span></li>");
-			    	   
-			    	    $("li.status[id='"+id+"'] span").find(":contains('http')").wrapInner("<a href='"+$("li.status[id='"+id+"'] span").find(":contains('http')").text()+"'> </a>");
-			    	    $("li.status[id='"+id+"'] span").find(":contains('@')").wrapInner("<a href='http://twitter.com/"+$("li.status[id='"+id+"'] span").find(":contains('@')").text().substr(1)+"'> </a>");
-			    	    $("li.status[id='"+id+"'] span").find(":contains('#')").wrapInner("<a href='http://twitter.com/#!/search/"+$("li.status[id='"+id+"'] span").find(":contains('#')").text().substr(1)+"'> </a>");
+					var newText = '<span>'+post.split(' ').join('</span> <span>')+'</span>';
+					$('ul.twitter-status').append("<li id='"+id+"' class='status'>&#187; <span class='post'>"+newText+"</span> <span class='stream-date'><a href='http://twitter.com/miguel_mota/status/"+id+"'>"+niceTime(date)+"</a></span></li>");
+					$("li.status[id='"+id+"'] span").find(":contains('http')").wrapInner("<a href='"+$("li.status[id='"+id+"'] span").find(":contains('http')").text()+"'> </a>");
+					$("li.status[id='"+id+"'] span").find(":contains('@')").wrapInner("<a href='http://twitter.com/"+$("li.status[id='"+id+"'] span").find(":contains('@')").text().substr(1)+"'> </a>");
+					$("li.status[id='"+id+"'] span").find(":contains('#')").wrapInner("<a href='http://twitter.com/#!/search/"+$("li.status[id='"+id+"'] span").find(":contains('#')").text().substr(1)+"'> </a>");
 					});
 					$('div.twitter div.loader').css('display','none');
 					showMtip('a.social.twitter');
