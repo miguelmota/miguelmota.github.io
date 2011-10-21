f4696a63b686494db819935415998537baecea78
Miguel Mota <miguelmota2@gmail.com>
2011-10-20 23:54:46 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -433,7 +433,7 @@ function streamPage(){
 		//initialize mtip
 		$('.mtip').mtip();
 		
-		showMtip('a.social.blog-stream');
+		showMtip('a.social.blog-social');
 		
 		// Twitter stream
 		$('div.twitter div.loader').css('display','block');
