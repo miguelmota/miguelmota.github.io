f9c81aa58893d7070c360d220b244fd1e7684ba7
Miguel Mota <miguelmota2@gmail.com>
2011-10-29 01:41:28 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -814,7 +814,7 @@ function contactPage(){
 		$('a.contact-submit').html('<span>sending...</span>');
 		var name = $('input#name').val();
 		$('form.contact-form').slideUp(300, function(){
-			$('.contact-form-thank-you').html('<p>Thank you <strong>'+name+'</strong>.</p><p>Your message has been successfully sent!<br />I will get in touch with you soon.</p>').fadeIn(1200);
+			$('.contact-form-thank-you').html('<p class="thank-you-name">Thank you <strong>'+name+'</strong>.</p><p>Your message has been successfully sent!<br />I will get in touch with you soon.</p>').fadeIn(1200);
 		});
 	}
 
