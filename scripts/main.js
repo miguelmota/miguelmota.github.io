5a319296f5c82d2efd6381fbbb3956ce4e7e9a33
Miguel Mota <miguelmota2@gmail.com>
2011-11-05 02:37:53 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -669,7 +669,7 @@ function streamPage(){
 							var url = item.url;
 							var name = item.name;
 							var artist = item.artist['#text'];
-							var image = item.image['#text'];
+							var image = item.image.size["small"];
 							var date =  item.date['#text'];
 							htmlString += "<li><a href='"+url+"'><img class='stream-thumb' src='"+image+"' alt='' /> "+artist+" - "+name+"</a> <time class='status-date'>"+date+"</time></li>";
 							$('.stream-lastfm').append(htmlString +'</ul>');
@@ -690,7 +690,7 @@ function streamPage(){
 				for(var i = 0; i < data.length; i++){
 					var entry = data[i].software;
 					var date = new Date(entry.last_active_at).toUTCString();
-					html.push("<li><a href='", entry.complete_url, "'> <img class='stream-thumb' style='width:16px;height:16px;min-height:8px;background:0;' src='", entry.complete_thumb_url ,"' alt='' /> ", entry.name, "</a> <time class='status-date'>"+niceTime(date)+"</time>", "</li>");
+					html.push("<li><a href='", entry.complete_url, "'> <img class='stream-thumb' src='", entry.complete_thumb_url ,"' alt='' /> ", entry.name, "</a> <time class='status-date'>"+niceTime(date)+"</time>", "</li>");
 				}
 				html.push("</ul>");
 				document.getElementById('stream-wakoopa-software').innerHTML = html.join("");
