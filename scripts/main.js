9130e919a39f8513bbea65ec5eba4483f4190af0
Miguel Mota <miguelmota2@gmail.com>
2011-11-05 14:19:42 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -187,24 +187,19 @@ $(document).ready(function(){
 	}
 	*/
 	
-	streamPage();
-	portfolioPage();
-	contactPage();
-	blogPage();
 	
 	
 	// add selected class based on the section block class
 	$('ul.main-nav li a').each(function(){ 
 		if($(this).attr('id') == $('section.content').attr('class').split(' ')[1]){
 			$(this).addClass('selected');
-			/*switch($('section.content').attr('class').split(' ')[1]){
+			switch($('section.content').attr('class').split(' ')[1]){
 				case 'stream':
 					streamPage();
 					break;
 				case 'about':
 					break;
 				case 'portfolio':
-				case 'portfolio/':
 					portfolioPage();
 					break;
 				case 'contact':
@@ -214,7 +209,7 @@ $(document).ready(function(){
 					blogPage();
 				default:
 					break;
-			}*/
+			}
 		
 		}
 	});
