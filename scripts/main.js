19b14d1d507601bcc8d09a05bc818bf6d614856a
Miguel Mota <miguelmota2@gmail.com>
2011-11-07 17:52:31 -0800

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -776,10 +776,10 @@ function streamPage(){
 						var flickr_photo = "http://farm"+flickr_farm+".static.flickr.com/"+flickr_server+"/"+flickr_id+"_"+flickr_secret+"_b.jpg";
 
 
-						htmlString += "<li><a class='fancybox' rel='flickr internal' href='"+flickr_photo+"' title='"+flickr_title+" ["+flickr_id+"]'><img src='"+flickr_thumbnail+"' alt='' /></a></li>";
+						htmlString += "<li><a class='fancybox' rel='flickr internal' href='"+flickr_photo+"' title='"+flickr_title+" ["+flickr_id+"]'><img src='"+flickr_thumbnail+"' alt='' /><span class='zoom-wrap'><span class='icon icon-zoom-24'></span></span></a></li>";
 					});
 					$('.stream-flickr div.loader').css('display','none');
-					$('.stream-flickr').append(htmlString +'</ul></div><a href="javascript:void(0);" class="stream-carousel-nav stream-carousel-nav-next fancybox"><span class="stream-carousel-nav-inner">&#187;</span><span class="zoom-wrap"><span class="icon icon-zoom-24"></span></span></a></div>');
+					$('.stream-flickr').append(htmlString +'</ul></div><a href="javascript:void(0);" class="stream-carousel-nav stream-carousel-nav-next"><span class="stream-carousel-nav-inner">&#187;</span></a></div>');
 					showMtipTimeout('.stream-logo-flickr');
 					
 					$('.stream-carousel-flickr').jCarouselLite({
