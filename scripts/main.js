acb639a077cf6720a2849663c6e522759d76bb7c
Miguel Mota <miguelmota2@gmail.com>
2011-11-08 21:54:23 -0800

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -29,15 +29,32 @@ $(document).ready(function(){
 			}
 		}
 		
-		// Initialize Masonry plugin, masonry.desandro.com
-		var $tumblelog = $('.content');
-		
-		$tumblelog.imagesLoaded( function(){
-		  $tumblelog.masonry({
-		    isFitWidth: true
-		  });
-		});
-		
+	});
+	
+	
+	
+	// Display latest tweet
+	$.getScript('http://miguelmota.tumblr.com/tweets.js');
+	function recent_tweets(data) {
+		for (i=0; i<1; i++) {
+			var date = new Date(data[i].created_at);
+			document.getElementById('tweets').innerHTML +=
+				'<div class="tweet-content"><a href="http://twitter.com/miguelmota/status/'+
+				+(data[i].id_str ? data[i].id_str : data[i].id)+'" rel="external">'+data[i].text+'</a> <time class="tweet-date">'+niceTime(data[i].created_at)+'</time></div>';
+		}
+		document.getElementById('tweet-wrap').style.display = 'block';
+	}
+	// Initialize latest tweet mtip
+	$('.mtip-tweet').mtip();
+	$('#tweet-wrap').live({
+		mouseenter:
+			function(){
+				showMtip('.icon-twitter-bird-24', this);
+		},
+		mouseleave:
+			function(){
+			hideMtip('.icon-twitter-bird-24', this);
+		}
 	});
 
 	
@@ -109,6 +126,17 @@ $(document).ready(function(){
 		$('.top-link').animate({top:offset},{duration:500,queue:false});
 	});
 	topYloc = parseInt($('.top-link').css('top').substring(0,$('.top-link').css('top').indexOf('px')));
+	
+	
+	
+	// Initialize Masonry plugin, masonry.desandro.com
+	var $tumblelog = $('.content');
+	
+	$tumblelog.imagesLoaded( function(){
+	  $tumblelog.masonry({
+	    isFitWidth: true
+	  });
+	});
 
 });
 
