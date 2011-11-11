74334abbc815e21dd3cdaa5db945b14fa53f548f
Miguel Mota <miguelmota2@gmail.com>
2011-11-10 22:13:53 -0800

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -796,16 +796,16 @@ function blogPage(){
 	$('.post-sort-grid').live('click', function(){
 		$('.post-sort-wrap a').removeClass('icon-no-hover icon-no-opacity');
 		$(this).addClass('icon-no-hover icon-no-opacity');
-		$('.blog-post-grid').slideUp('fast');
-		$('.blog-post-list').slideDown('fast');
+		$('.blog-post-list').slideUp('fast');
+		$('.blog-post-grid').slideDown('fast');
 	});
 	
 	// Show list sort
 	$('.post-sort-list').live('click', function(){
 		$('.post-sort-wrap a').removeClass('icon-no-hover icon-no-opacity');
 		$(this).addClass('icon-no-hover icon-no-opacity');
-		$('.blog-post-list').slideUp('fast');
-		$('.blog-post-grid').slideDown('fast');
+		$('.blog-post-grid').slideUp('fast');
+		$('.blog-post-list').slideDown('fast');
 	});
 	
 	
