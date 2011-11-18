b4ec7195e05510bdc2f2b079077c54be697c6713
Miguel Mota <miguelmota2@gmail.com>
2011-11-17 17:29:07 -0800

    Update scripts/main.js

--- scripts/main.js
+++ scripts/main.js
@@ -811,7 +811,7 @@ function portfolioPage(){
 	$('.project').live({
 		mouseenter:
 			function(){
-				jQuery('.overlay-popup', this).animate({bottom: '0'}, {queue: false, duration: 150});
+				jQuery('.overlay-popup', this).animate({bottom: '-20px'}, {queue: false, duration: 150});
 		},
 		mouseleave:
 			function(){
