df32cc408f5a5a3217ace1507b758ebab9796f99
Miguel Mota <miguelmota2@gmail.com>
2011-11-21 15:22:44 -0800

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -624,7 +624,10 @@ function streamPage(){
 						var url = item.url;
 						var name = item.name;
 						var artist = item.artist['name'];
-						var image = "item.image[0]['#text']";
+						var image = '';
+						if (item.image[0]['#text']) {
+							image = item.image[0]['#text'];
+						}
 						var playcount =  item.playcount;
 						htmlString += "<li><a href='"+url+"' rel='external'><img class='stream-thumb' src='"+image+"' alt='' /> "+artist+" - "+name+"</a> <time class='status-date'>"+playcount+" plays</time></li>";
 						$('.stream-lastfm').append(htmlString +'</ul>');
