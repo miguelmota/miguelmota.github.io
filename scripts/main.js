4acb78e6bef41a813f3f5348d7fed5c73495c921
Miguel Mota <miguelmota2@gmail.com>
2011-12-01 02:18:02 -0800

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -1258,6 +1258,34 @@ function blogPage(){
 
 }
 
+
+
+// Prevent variables from being global
+(function () {
+
+      /*
+          1. Inject CSS which makes iframe invisible
+      */
+
+    var div = document.createElement('div'),
+        ref = document.getElementsByTagName('base')[0] ||
+              document.getElementsByTagName('script')[0];
+
+    div.innerHTML = '&shy;<style> iframe { visibility: hidden; } </style>';
+
+    ref.parentNode.insertBefore(div, ref);
+
+    /*
+        2. When window loads, remove that CSS,
+           making iframe visible again
+    */
+
+    window.onload = function() {
+        div.parentNode.removeChild(div);
+    }
+
+})();
+
 /*
  * --------------------------------------------------------------------------------
  *	END FUNCTIONS
