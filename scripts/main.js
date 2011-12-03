9ab0e148746a808bd6cb9296ac382c5b0b0b8478
Miguel Mota <miguelmota2@gmail.com>
2011-12-02 23:21:27 -0800

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -368,7 +368,7 @@ function initializeFancybox() {
 // Initialize about page map
 function initializeAboutMap() {
 
-	var latlng = new google.maps.LatLng(33.934815,-117.547703);
+	var latlng = new google.maps.LatLng(-117.547703,33.934815,);
 	var myOptions = {
 			zoom: 5,
 			center: latlng,
