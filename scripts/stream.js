c93d65a40844264a1c1efb7754349ef9b55a0a4a
Miguel Mota <miguelmota2@gmail.com>
2012-04-17 00:36:31 -0700

    iframe white flash updated

--- scripts/stream.js
+++ scripts/stream.js
@@ -481,11 +481,8 @@ $(document).ready(function(){
 	// Hide white flash on iframe load
 	// Prevent variables from being global
 	(function () {
-
-	      ///
-	          1. Inject CSS which makes iframe invisible
-	      ///
-
+	   
+	    // 1. Inject CSS which makes iframe invisible
 	    var div = document.createElement('div'),
 	        ref = document.getElementsByTagName('base')[0] ||
 	              document.getElementsByTagName('script')[0];
@@ -494,11 +491,7 @@ $(document).ready(function(){
 
 	    ref.parentNode.insertBefore(div, ref);
 
-	    ///
-	        2. When window loads, remove that CSS,
-	           making iframe visible again
-	    ///
-
+	    // 2. When window loads, remove that CSS, making iframe visible again
 	    window.onload = function() {
 	        div.parentNode.removeChild(div);
 	    }
