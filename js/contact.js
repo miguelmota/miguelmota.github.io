68c931354d12188796dce971348da57f58807251
Miguel Mota <miguelmota2@gmail.com>
2013-02-05 23:07:37 -0800

    updated contact ajax url

--- js/contact.js
+++ js/contact.js
@@ -11,9 +11,9 @@ $(document).ready(function(){
 		$.validator.addMethod('nameCheck', function(value, element){
 			return this.optional(element) || /^[a-zA-Z\s]*$/.test(value);
 	});
-		
 
-		
+
+
 	// Validate contact form
 	$('.contact-form').validate({
 		rules: {
@@ -46,30 +46,29 @@ $(document).ready(function(){
 		},
 		onkeyup: true,
 		success: function(label) {
-			label.addClass('valid');	
+			label.addClass('valid');
 		}
 	});
-	
-	
-	
+
+
+
 	// If contact form validates to true then submit it
-	if ($('.contact-form').valid() == true){	
-		
+	if ($('.contact-form').valid() == true){
+
 		$('.contact-form-submit').html('<span>sending...</span>');
-		
+
 		var str = $('.contact-form').serialize();
-		
+
 		$.ajax({
 			type: 'get',
-			url: 'http://www.foodfail.org/miguelmota/contact.php',
-			// url: 'http://miguelmota.webuda.com/contact/contact.php',
+			url: 'http://miguelmota.pagodabox.com/models/mailer.php',
 			data: str,
 			success: function(){
 
 				sendSuccess();
 			},
 			error: function(){
-				
+
 				sendError();
 			}
 		});
@@ -78,9 +77,9 @@ $(document).ready(function(){
 	else
 		return false;
 	});
-	
-	
-	
+
+
+
 	// Hide contact form and display thank you message
 	function sendSuccess(){
 		var name = $('#contact-form-name').val();
@@ -88,14 +87,14 @@ $(document).ready(function(){
 			$('.contact-form-thank-you').html('<p class="thank-you-name">Thank you, <strong>'+name+'</strong>.</p><p>Your message has been successfully sent <span class="icon icon-checkmark-16 icon-no-hover icon-no-opacity"></span><br />I will get in touch with you soon.</p>').fadeIn(1200);
 		});
 	}
-	
-	
-	
+
+
+
 	// Display error message submit failed
 	function sendError(){
 		$('.contact-form').slideUp(300, function(){
 			$('.contact-form-thank-you').html('<p>Sorry, there was an error. Message was not sent.</p><p>Email <a href="mailto:hello@miguelmota.com">hello@miguelmota.com</a>?</p>');
 		});
 	}
-	
+
 });
