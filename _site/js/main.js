bb649ccb1595b0615700627d92585bea507002d3
Miguel Mota <miguelmota2@gmail.com>
2013-04-05 01:32:01 -0700

    converted to foundation

---
+++ _site/js/main.js
@@ -0,0 +1,15 @@
+//main.js
+
+var MM = MM || (function() {
+
+	return {
+		initialize: function() {
+			$(document).foundation();
+		}
+	}
+
+})();
+
+$(document).ready(function() {
+	MM.initialize();
+});
